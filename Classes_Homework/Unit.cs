7b81838eebf812f4af563cbf04240b94a5cd240f
SynthIris <irinasovav@gmail.com>
2025-10-15 14:45:31 +0300

    Classes_Homework

---
+++ Classes_Homework/Unit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Homework
+{
+    public class Unit
+    {
+        private float _health;
+        public string Name { get; }                    
+        public float Health => _health;                
+        public int Damage { get; }                    
+        public float Armor { get; }
+        public Unit() : this("Unknown Unit")          
+        {
+           
+        }
+
+        public Unit(string name)                      
+        {
+            Name = name;
+            _health = 100f;     
+            Damage = 5;         
+            Armor = 0.6f;     
+        }
+
+        public float GetRealHealth()                   
+        {
+            return _health * (1f + Armor);  
+        }
+
+        public bool SetDamage(float value)             
+        {
+            _health = _health - value * Armor;
+            return _health <= 0f;
+        }
+
+    }
+}
